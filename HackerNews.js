efb89f7fc050d8ec91860858f3b76ab475d56857
Chris Taylor <zipcraftvalentine@gmail.com>
2018-09-26 12:01:28 -0700

    Code is now functional after TJ edits

--- HackerNews.js
+++ HackerNews.js
@@ -2,20 +2,27 @@ let url = "https://newsapi.org/v2/top-headlines?sources=hacker-news&apiKey=31829
 
 fetch(url)
 .then(r => {
-    return r.json();
+    return r.json();  
 })
 
 .then(data => {
-    let articles = data.articles;
+  console.log(data);
+    //console.log(Object.keys(data.articles[0]));
     let headlines = document.createElement("ul");
     let body = document.querySelector("body");
+    let count = document.createElement('h2');
+    count.innerHTML = "Total Results: "+data.totalResults;
+    body.appendChild(count);
     body.appendChild(headlines);
-    articles.map(sources => {
+    data.articles.map(article => {
         let newsArticle = document.createElement("li");
         newsArticle.innerHTML = 
-        '<a href="' + articles.href + '">' + articles.title + "</a>";
+        '<a href="' + article.url + '" target="_blank">' + article.title +"</a>";
         headlines.appendChild(newsArticle);
+        
     });
+  
+  
 })
 .catch(err => {
     console.log(`An error occurred: ${err}`);
